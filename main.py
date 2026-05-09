fffcea57edbe861cf7da39edc196b1321db35d8d
axon4 <65050809+axon4@users.noreply.github.com>
2026-05-09 18:23:40 +0100

    print prompt & response token-counts from usage metadata

--- main.py
+++ main.py
@@ -8,12 +8,20 @@ def main():
     api_key = os.environ.get('GEMINI_API_KEY')
 
     if not api_key:
-        raise RuntimeError('error finding API Key')
+        raise RuntimeError('error finding Gemini API Key')
 
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(model='gemini-2.5-flash', contents='lorem ipsum dolor sit amet, consectetur adipiscing elit')
+    prompt = 'lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    response = client.models.generate_content(model='gemini-2.5-flash', contents=prompt)
 
+    if response.usage_metadata == None:
+        raise RuntimeError('error contacting Gemini API')
+
+    print(f'prompt: {prompt}')
+    print(f'prompt tokens: {response.usage_metadata.prompt_token_count}')
+    print(f'response tokens: {response.usage_metadata.candidates_token_count}')
+    print(f'response:')
     print(response.text)
 
 
