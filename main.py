984713c487077f307b80481ba9f9f5e613577839
axon4 <65050809+axon4@users.noreply.github.com>
2026-05-09 18:31:54 +0100

    parse prompt from arguments

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from dotenv import load_dotenv
 import os
 from google import genai
+import argparse
 
 
 def main():
+    argument_parser = argparse.ArgumentParser(description='Code Agent')
+    argument_parser.add_argument('prompt', type=str, help='<prompt>')
+    arguments = argument_parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get('GEMINI_API_KEY')
 
@@ -12,16 +17,15 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    prompt = 'lorem ipsum dolor sit amet, consectetur adipiscing elit'
-    response = client.models.generate_content(model='gemini-2.5-flash', contents=prompt)
+    response = client.models.generate_content(model='gemini-2.5-flash', contents=arguments.prompt)
 
     if response.usage_metadata == None:
         raise RuntimeError('error contacting Gemini API')
 
-    print(f'prompt: {prompt}')
-    print(f'prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'response tokens: {response.usage_metadata.candidates_token_count}')
-    print(f'response:')
+    print(f'prompt: {arguments.prompt}')
+    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    print(f'Response:')
     print(response.text)
 
 
