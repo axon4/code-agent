434db7181c549bc60649b354bb1ac30164fd40da
axon4 <65050809+axon4@users.noreply.github.com>
2026-05-09 18:11:14 +0100

    install DotEnv and Gemini, integrate Gemini LLM

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from dotenv import load_dotenv
+import os
+from google import genai
+
+
 def main():
-    print('hello from code-agent!')
+    load_dotenv()
+    api_key = os.environ.get('GEMINI_API_KEY')
+
+    if not api_key:
+        raise RuntimeError('error finding API Key')
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(model='gemini-2.5-flash', contents='lorem ipsum dolor sit amet, consectetur adipiscing elit')
+
+    print(response.text)
 
 
 if __name__ == '__main__':
